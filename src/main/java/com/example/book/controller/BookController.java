44479b77452e108bde6f999f20c05825bd4c1a03
manaf_ceferov <ceferovmanaf1@gmail.com>
2025-10-10 20:43:25 +0400

    Initial commit

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -3,7 +3,6 @@ package com.example.book.controller;
 import com.example.book.dto.*;
 import com.example.book.service.BookService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
